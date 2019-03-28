737cb906aec668623c38f8cb523a33d8004491c8
github@finnesand.no <github@finnesand.no>
2019-03-28 14:46:12 +0100

    change repName

--- index.js
+++ index.js
@@ -9,13 +9,16 @@ const port = 8080;
 const githubUsername = 'secretlifeof';
 
 const getCDFile = async req => {
+  const repository = req && req.body && req.body.repository;
+  const repName = repository && repository.full_name;
+
   const CDFile = await curl
     .setHeaders([
       'Authorization: token cac68f8634993293941b98a82bf653dbbccb57e4',
       'Accept: application/vnd.github.v3.raw',
       'User-Agent: secretlifeof'
     ])
-    .get('https://api.github.com/repos/secretlifeof/teuberkohlhoff/contents/LittleCD.yaml');
+    .get(`https://api.github.com/repos/${repName}/contents/LittleCD.yaml`);
   const fileToObject = yaml.safeLoad(CDFile.body);
 
   return fileToObject;
@@ -31,9 +34,6 @@ app.post('/webhooks/github', async (req, res) => {
   const sender = (req && req.body && req.body.sender) || 'githubUsername';
   const branch = (req && req.body && req.body.ref) || 'master';
   const wantedBranch = 'master';
-
-  const repository = req && req.body && req.body.repository;
-  const repName = repository && repository.full_name;
 
   // res.send(await getCDFile(req));
 
