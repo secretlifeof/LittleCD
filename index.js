69e7535d9d467caf83406a64d1ec1db16f215471
github@finnesand.no <github@finnesand.no>
2019-05-10 11:22:10 +0200

    added git stash

--- index.js
+++ index.js
@@ -1,54 +1,56 @@
-var bodyParser = require('body-parser');
-var express = require('express');
-var app = express();
-require('dotenv').config();
+var bodyParser = require('body-parser')
+var express = require('express')
+var app = express()
+require('dotenv').config()
 // var childProcess = require('child_process');
-var shell = require('shelljs');
-const curl = new (require('curl-request'))();
-yaml = require('js-yaml');
+var shell = require('shelljs')
+const curl = new (require('curl-request'))()
+yaml = require('js-yaml')
 
-const port = process.env.CD_PORT || 8080;
-const githubUsername = process.env.GITHUB_USERNAME || 'secretlifeof';
+const port = process.env.CD_PORT || 8080
+const githubUsername = process.env.GITHUB_USERNAME || 'secretlifeof'
 
 const getCDFile = async req => {
-  const repository = req && req.body && req.body.repository;
-  const repName = repository && repository.full_name;
+	const repository = req && req.body && req.body.repository
+	const repName = repository && repository.full_name
 
-  const CDFile = await curl
-    .setHeaders([
-      `Authorization: token ${process.env.ACCESS_TOKEN}`,
-      'Accept: application/vnd.github.v3.raw',
-      'User-Agent: secretlifeof'
-    ])
-    .get(`https://api.github.com/repos/${repName}/contents/LittleCD.yaml`);
-  const fileToObject = yaml.safeLoad(CDFile.body);
+	const CDFile = await curl
+		.setHeaders([
+			`Authorization: token ${process.env.ACCESS_TOKEN}`,
+			'Accept: application/vnd.github.v3.raw',
+			'User-Agent: secretlifeof'
+		])
+		.get(`https://api.github.com/repos/${repName}/contents/LittleCD.yaml`)
+	const fileToObject = yaml.safeLoad(CDFile.body)
 
-  return fileToObject;
-};
+	return fileToObject
+}
 
-const executeShellCommands = commandList => { 
-  commandList.forEach(command => {
-    shell.exec(command);
-  });
-};
+const executeShellCommands = commandList => {
+	commandList.forEach(command => {
+		shell.exec(command)
+	})
+}
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.post('/webhooks/github', async (req, res) => {
-  const sender = req && req.body && req.body.sender;
-  const branch = req && req.body && req.body.ref;
-  const wantedBranch = 'master';
-  const repository = req && req.body && req.body.repository;
-  const repName = repository && repository.full_name;
+	const sender = req && req.body && req.body.sender
+	const branch = req && req.body && req.body.ref
+	const wantedBranch = 'master'
+	const repository = req && req.body && req.body.repository
+	const repName = repository && repository.full_name
 
-  if (branch.includes(wantedBranch) && sender.login === githubUsername) {
-    const yaml = await getCDFile(req);
-    shell.exec(`git -C /srv/www/teuberkohlhoff/ pull https://${process.env.ACCESS_TOKEN}@github.com/${repName}`);
-    const shellCommands = yaml && yaml.pipeline.commands;
-    shellCommands && executeShellCommands(shellCommands);
-  }
-});
+	if (branch.includes(wantedBranch) && sender.login === githubUsername) {
+		const yaml = await getCDFile(req)
+		shell.exec(
+			`git stash && git -C /srv/www/teuberkohlhoff/ pull https://${process.env.ACCESS_TOKEN}@github.com/${repName}`
+		)
+		const shellCommands = yaml && yaml.pipeline.commands
+		shellCommands && executeShellCommands(shellCommands)
+	}
+})
 
-app.get('/', (req, res) => res.send('Hello LittleCD!'));
+app.get('/', (req, res) => res.send('Hello LittleCD!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
